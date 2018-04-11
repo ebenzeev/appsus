ae80923d1d8ec10cc4adb92a08a240490a41818a
Eyal Ben Zeev <eyal.benzeev@gmail.com>
2018-04-11 23:08:29 +0300

    rendering email preview with all elements

--- js/cmps/mister-email/email-details.js
+++ js/cmps/mister-email/email-details.js
@@ -1,6 +1,17 @@
 export default {
     props: ['email'],
     template: `
-        <h2>{{ email.subject }}</h2>
+        <section>
+          <div>
+            <button> Compose </button>
+            <div>
+                <h2>{{ email.subject }}</h2>
+                <p>From: {{ email.sendFrom.name }} | {{ email.sendFrom.email }} </p>
+                <p>Sent: {{ email.sentAt }}</p>
+                <hr>
+                <p>{{ email.body }}</p>
+            </div>
+            </div>
+        </section>
     `
 }
