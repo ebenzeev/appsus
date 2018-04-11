8c040f2c5371fecf3854933c56fb551c5b8055e6
Eyal Ben Zeev <eyal.benzeev@gmail.com>
2018-04-11 16:30:30 +0300

    adding nested route + remoivng history moded from html

--- js/pages/mister-email/email.home.js
+++ js/pages/mister-email/email.home.js
@@ -4,6 +4,9 @@ export default {
     template: `
     <section class="email-home">
         <h1>Mister Email App!</h1>
+        <!-- <email-list></email-list> -->
+        <router-view></router-view>
+        <!-- <emial-status></email-status> -->
     </section>
     `,
     data() {
--- js/routes.js
+++ js/routes.js
@@ -1,12 +1,19 @@
 import home from './pages/appsus.home.js';
 import emailHome from './pages/mister-email/email.home.js';
+import emailCompose from './cmps/mister-email/email-compose.js';
+import emailDetails from './cmps/mister-email/email-details.js';
 
 const routes = [
     {path: '/', component: home},
-    {path: '/email', component: emailHome}
+    {path: '/email', component: emailHome,
+    children: [
+        {path: 'compose', component: emailCompose},
+        {path: 'details/:id', component: emailDetails}      
+    ]    
+}
 ];
 
 Vue.use(VueRouter);
-var myRouter = new VueRouter({mode:'history' ,routes});
+var myRouter = new VueRouter({routes});
 
 export default myRouter;
