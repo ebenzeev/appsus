168019836d64c9eee1b8fa5b0382521fa02f0703
Orel star <orelstar@h-MacBook-Pro-sl-Orel.local>
2018-04-12 13:52:51 +0300

    before mareging - adding style and

--- js/cmps/mister-email/email-list.js
+++ js/cmps/mister-email/email-list.js
@@ -3,8 +3,8 @@ import emailPreview from '../../cmps/mister-email/email-preview.js';
 export default {
     props:['emails'],
     template: `
-        <section class="">
-            <nav class="email-list">
+        <section class="email-list">
+            <nav class="email-list-items">
                 <ul>
                     <li v-for="(email,idx) in emails">
                         <email-preview :email="email" @click.native="emitSelected(idx)"></email-preview>
--- js/pages/appsus.home.js
+++ js/pages/appsus.home.js
@@ -1,7 +1,13 @@
 export default {
     template: `
     <section class="home">
-        <h1>Home!</h1>
+        <header>
+            <h1>Welcome to appSuS!</h1>
+        <header>
+<!-- 
+        <main></main>
+
+        <footer></fotter> -->
     </section>
     `
 }
